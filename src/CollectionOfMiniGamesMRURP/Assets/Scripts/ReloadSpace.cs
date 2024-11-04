09a149eb4377a3a356951bdb212c20c64b11d1b8
Kirill Kornilov <99721154+krl76@users.noreply.github.com>
2024-11-04 19:10:47 +0300

    init

---
+++ src/CollectionOfMiniGamesMRURP/Assets/Scripts/ReloadSpace.cs
@@ -0,0 +1,39 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ReloadSpace : MonoBehaviour
+{
+    [SerializeField] private GameObject _sceneMeshBlock;
+    
+    private InputController _inputController;
+    private GameObject _sceneMesh;
+
+    private void Awake()
+    {
+        _sceneMesh = Instantiate(_sceneMeshBlock, new Vector3(0, 0, 0), quaternion.identity);
+        
+        _inputController = new InputController();
+
+        _inputController.Interaction.ReloadSpace.started += ctx =>
+        {
+            Destroy(_sceneMesh);
+            Invoke(nameof(SpawnSceneMesh), 10f);
+            _ = OVRScene.RequestSpaceSetup();
+        };
+    }
+
+    private void SpawnSceneMesh()
+    {
+        _sceneMesh = Instantiate(_sceneMeshBlock);
+    }
+    
+    private void OnEnable()
+    {
+        _inputController.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputController.Disable();
+    }
+}
---
+++ src/CollectionOfMiniGamesMRURP/Assets/Scripts/ReturnObject.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnObject : MonoBehaviour
+{
+    private float _spawnDistanceFromCamera = 0.75f;
+    private OVRCameraRig _cameraRig;
+    
+    private InputController _inputController;
+    
+    private void Awake()
+    {
+        _cameraRig = FindObjectOfType<OVRCameraRig>();
+        
+        _inputController = new InputController();
+
+        _inputController.Interaction.ReturnUI.started += ctx => StartCoroutine(SnapCanvasInFrontOfCamera());
+    }
+    
+    private void OnEnable()
+    {
+        _inputController.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputController.Disable();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SnapCanvasInFrontOfCamera());
+    }
+
+    private IEnumerator SnapCanvasInFrontOfCamera()
+    {
+        yield return new WaitUntil(
+            () => _cameraRig && _cameraRig.centerEyeAnchor.transform.position != Vector3.zero);
+        transform.position = _cameraRig.centerEyeAnchor.transform.position +
+                             _cameraRig.centerEyeAnchor.transform.forward * _spawnDistanceFromCamera;
+    }
+}
